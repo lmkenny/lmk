4371e4634187f3c0bb08c5abd670b93a644812a6
lmkenny <lmkenly@gmail.com>
2018-11-01 22:03:41 +1100

    Undo js change

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
-
-var imgs = document.querySelectorAll('img'), i;
-var wrappers = document.getElementsByClassName('loading-image-color');
-
-for (i = 0; i < imgs.length; ++i) {
-  if (imgs[i].complete) {
-    imgs[i].style.opacity = "1";
-    wrappers[i].style.backgroundColor = "white";
-  } else {
-  };
-}
+// 
+// var imgs = document.querySelectorAll('img'), i;
+// var wrappers = document.getElementsByClassName('loading-image-color');
+//
+// for (i = 0; i < imgs.length; ++i) {
+//   if (imgs[i].complete) {
+//     imgs[i].style.opacity = "1";
+//     // wrappers[i].style.backgroundColor = "white";
+//   } else {
+//   };
+// }
